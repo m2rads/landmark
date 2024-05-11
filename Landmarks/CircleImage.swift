f7e1246150ed11748081e360873df092cd331f0c
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-11 12:27:11 -0700

    add landmark images for the turtle park

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Landmarks
+//
+//  Created by m2rads on 2024-05-11.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
