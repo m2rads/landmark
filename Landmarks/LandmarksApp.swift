5961f62f96f2418b750e25873f27ca6e797460af
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-11 10:57:26 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by m2rads on 2024-05-11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
