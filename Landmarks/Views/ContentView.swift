3ddf07aa5ec6e08605923586139e23ee5e22f244
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-16 11:16:32 -0700

    Add landmarklist view to show as the entry point for the app

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,32 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            CircleImageView()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            VStack(alignment: .leading) {
-                Text("AccessNotes")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                HStack {
-                    Text("A web annotation tool that lives in your pocket:")
-                    Spacer()
-                    Text("IOS 15")
-                }
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
-
-                Divider()
-                
-                Text("About AccessNotes")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                Text("Easily make highlights and turn them into redescoverable notes")
-            }
-            .padding()
-        }
-        Spacer()
+        LandmarkList()
     }
     
 }
---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by m2rads on 2024-05-16.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
