692aa9218c382dee05ebf097104772d39b13fae4
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-16 11:16:45 -0700

    add landmark detail view

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImageView()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("AccessNotes")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Text("A web annotation tool that lives in your pocket:")
+                    Spacer()
+                    Text("IOS 15")
+                }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+
+                Divider()
+                
+                Text("About AccessNotes")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("Easily make highlights and turn them into redescoverable notes")
+            }
+            .padding()
+        }
+        Spacer()
     }
 }
 
