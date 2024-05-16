94bba9b4f2393c33256466aba018106bafda9dbb
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-16 11:10:49 -0700

    add landmark rowv view groups

--- Landmarks/Views/LandmarkRowView.swift
+++ Landmarks/Views/LandmarkRowView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct LandmarkRowView: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            // this is because inside landmark model we are initating the image as swift ui image
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
     }
 }
 
+// we need a landmark instance during initilization
+// This is due to the stored landmark propery at the top
 #Preview {
-    LandmarkRowView()
+    Group {
+        LandmarkRowView(landmark: landmarks[0])
+        LandmarkRowView(landmark: landmarks[1])
+    }
 }
