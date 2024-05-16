e89cb8f1940e51a26a21454f3663d889ff449503
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-16 11:10:15 -0700

    add identifiable protocol to landmark

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by m2rads on 2024-05-16.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
---
+++ Landmarks/Views/LandmarkRowView.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRowView.swift
+//  Landmarks
+//
+//  Created by m2rads on 2024-05-15.
+//
+
+import SwiftUI
+
+struct LandmarkRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRowView()
+}
