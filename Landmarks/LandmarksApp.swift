ee0ab25e63d42057d006f8028d94f6f8e606b9de
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-16 21:42:09 -0700

    add environment for landmark details view to get the model data

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    @State private var modelData = ModelData() // this state is only inititialized once during the life time of an application
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
         }
     }
 }
